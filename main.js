b1b70e180a32cc7acbb0eefec4dfa5665313f424
burmatov-step <stepaburmatov@mail.ru>
2020-08-10 12:27:59 +0300

    end 3 less

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-let num = 266219;
-let numStr = num.toString();
-let numPr = 1
-for(let i = 0; i< numStr.length; i++){
-    numPr *= Number(numStr[i])
-}
-let three = numPr ** 3;
+'use strict'
+// Первое задание
+let ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let days = [ru, en];
 
-console.log(Number(three.toString().substring(0,2)))
+let lang = 'en';
+// Через IF
+if(lang === 'ru'){
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en'){
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+// Через switch
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        break;
+    case 'en':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break;
+    default:
+        console.log('Выберите язык')
+        break;
+}
+// Через тернарный оператор и многомерный массив
+lang === 'ru' ? console.log(days[0]) : console.log(days[1])
+
+
+// Второе задание
+let namePerson = 'Степан';
+
+namePerson === 'Артем' ? console.log('Директор') : 
+namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Ученик');
+ 
+
