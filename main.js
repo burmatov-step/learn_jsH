2b2a3eb80a364037c57866b57c99c35ff7302cda
burmatov-step <stepaburmatov@mail.ru>
2020-08-11 11:31:54 +0300

    end lesson

--- main.js
+++ main.js
@@ -1,42 +1,13 @@
 'use strict'
-// Первое задание
-let ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-
-let lang = prompt('Выберите язык, "ru" или "en"', 'ru');
-// Через IF
-if(lang === 'ru'){
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} else if (lang === 'en'){
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+function sss(str) {
+  if(typeof str !== 'string'){
+    alert('Введите строку');
+    return
+  } else if(str.length > 30){
+    return str.trim().substring(0, 30) + '...'
+  } else{
+    return str.trim();
+  }
 }
-// Через switch
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-        break;
-    case 'en':
-        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-        break;
-    default:
-        console.log('Выберите язык')
-        break;
-}
-// Через тернарный оператор и многомерный массив
-
-let object = {
-    'ru' : ru,
-    'en' : en,
-}
-
-console.log(object[lang]);
-
-
-// Второе задание
-let namePerson = 'Степан';
-
-namePerson === 'Артем' ? console.log('Директор') :
-namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Ученик');
-
 
