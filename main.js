68b22e074f9d4e25fd773f332d5f37ba57292465
burmatov-step <stepaburmatov@mail.ru>
2020-08-14 13:38:12 +0300

    end lesson

--- main.js
+++ main.js
@@ -1,49 +1,26 @@
 "use strict";
 
 
+let days = [
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
+];
 
-let isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
+let week = days.join();
 
-let start = numberPlay();
-start();
+document.write(`${week} </br>`);
 
-function numberPlay(x = 9) {
-  let num = Math.ceil(Math.random() * 100);
-  return function number() {
-    let ourNumber = prompt("Угадай число от 1 до 100");
-    if (ourNumber === null) {
-      alert("Возвращайтесь еще!");
-      return;
-    } else if (!isNumber(ourNumber)) {
-      alert("Введите число");
-      number();
-    } else if (+ourNumber > num && x !== 0) {
-      alert(`Загаданное число меньше, осталось попыток ${x}`);
-      x--;
-      number();
-    } else if (+ourNumber < num && x !== 0) {
-      alert(`Загаданное число больше, осталось попыток ${x}`);
-      x--;
-      number();
-    } else if (+ourNumber === num) {
-     let good =  confirm("Поздравлю, вы угадали!!! Хотели бы сыграть еще?");
-      if (good === true) {
-        numberPlay()();
-      } else {
-        return;
-      }
-    } else if (x === 0) {
-     let end =  confirm("Попытки закончились, хотите сыграть еще?");
-     if(end === true){
-       numberPlay()();
-     } else{
-       return;
-     }
 
-    }
-  };
-}
+week.split(",").forEach(function (item) {
+  document.write(`${item} </br>`)
+});
 
+document.write(week.split(",").slice(-2).join().italics() + '</br>');
+let now = new Date()
+document.write(week.split(",")[now.getDay() - 1].bold());
 
