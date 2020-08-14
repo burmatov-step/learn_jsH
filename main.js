40b9a6834586c151e185c7464d77b25501fe59a6
burmatov-step <stepaburmatov@mail.ru>
2020-08-14 18:59:21 +0300

    remark

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-let days = [
+let week = [
   "Понедельник",
   "Вторник",
   "Среда",
@@ -11,16 +11,26 @@ let days = [
   "Воскресенье",
 ];
 
-let week = days.join();
 
-document.write(`${week} </br>`);
+week.forEach(function (item, index) {
+  if(item === 'Суббота' || item === 'Воскресенье'){
+    document.write(`${item.italics()} </br>`);
+  } else if(index === new Date().getDay() - 1 ){
+    document.write(`${item.bold()} </br>`);
+  } else{
+    document.write(`${item} </br>`);
+  }
+
+})
+// let week = days.join();
+
+// document.write(`${week} </br>`);
 
 
-week.split(",").forEach(function (item) {
-  document.write(`${item} </br>`)
-});
+// week.split(",").forEach(function (item) {
+//   document.write(`${item} </br>`)
+// });
 
-document.write(week.split(",").slice(-2).join().italics() + '</br>');
-let now = new Date()
-document.write(week.split(",")[now.getDay() - 1].bold());
-
+// document.write(week.split(",").slice(-2).join().italics() + '</br>');
+// let now = new Date()
+// document.write(week.split(",")[now.getDay() - 1].bold());
