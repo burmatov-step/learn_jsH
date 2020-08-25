7ba057f3beb6ae38f8308edb19787a3e31bb6958
burmatov-step <stepaburmatov@mail.ru>
2020-08-25 13:13:50 +0300

    remark

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const DomElement = function (selector, height, width, bg, fontSize) {
   this.selector = selector;
   this.height = height;
@@ -10,13 +11,14 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 DomElement.prototype.create = function () {
   let el;
   let inp = document.createElement("input");
-  if(this.selector[0] === '.'){
-   el =  document.createElement('div');
-
-  } else if(this.selector[0] === '#'){
-     el = document.createElement("p");
-  } else{
-    return
+  if (this.selector[0] === ".") {
+    el = document.createElement("div");
+    el.classList.add(this.selector.substring(1));
+  } else if (this.selector[0] === "#") {
+    el = document.createElement("p");
+    el.id = this.selector.substring(1);
+  } else {
+    return;
   }
   el.style.height = this.height + "px";
   el.style.width = this.width + "px";
@@ -24,13 +26,16 @@ DomElement.prototype.create = function () {
   el.style.fontSize = this.fontSize + "px";
   document.body.append(el);
   document.body.append(inp);
-  inp.addEventListener('keyup', function () {
-    el.textContent = inp.value
-  })
-
+  inp.addEventListener("keyup", function () {
+    el.textContent = inp.value;
+  });
 };
 
+const appData = new DomElement("#fff", 100, 200, "blue", 20);
 
-const appData = new DomElement('#fff', 100, 200, 'blue', 20);
+appData.create();
 
-appData.create()
+
+
+
+
